29ebb361ece1af00a6732c71fc92eb2734d99f03
Nathan Crouther <nathan.crouther@abbott.com>
2016-06-28 09:28:48 -0700

    Chapter 6 - implementation

--- app/src/main/java/com/bignerdranch/android/initialnerdmart/NerdMartAbstractFragment.java
+++ app/src/main/java/com/bignerdranch/android/initialnerdmart/NerdMartAbstractFragment.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.initialnerdmart;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,13 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.bignerdranch.android.nerdmartservice.service.NerdMartServiceInterface;
+import com.bignerdranch.android.initialnerdmart.model.service.NerdMartServiceManager;
 
 import javax.inject.Inject;
 
+import rx.Observable;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
 public abstract class NerdMartAbstractFragment extends Fragment {
     @Inject
-    NerdMartServiceInterface mNerdMartServiceInterface;
+    NerdMartServiceManager mNerdMartServiceManager;
+    private CompositeSubscription mCompositeSubscription;
+    private ProgressDialog mDialog;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,11 +32,33 @@ public abstract class NerdMartAbstractFragment extends Fragment {
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mCompositeSubscription = new CompositeSubscription();
+        setupLoadingDialog();
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        mCompositeSubscription.clear();
+    }
+
+    protected void addSubscription(Subscription subscription) {
+        mCompositeSubscription.add(subscription);
+    }
+
+    private void setupLoadingDialog() {
+        mDialog = new ProgressDialog(getActivity());
+        mDialog.setIndeterminate(true);
+        mDialog.setMessage(getString(R.string.loading_text));
+    }
+
+    protected <T> Observable.Transformer<T, T> loadingTransformer() {
+        return observable -> observable.doOnSubscribe(mDialog::show)
+                .doOnCompleted(() -> {
+                    if (mDialog != null && mDialog.isShowing()) {
+                        mDialog.dismiss();
+                    }
+                });
     }
 }
--- app/src/main/java/com/bignerdranch/android/initialnerdmart/inject/NerdMartApplicationModule.java
+++ app/src/main/java/com/bignerdranch/android/initialnerdmart/inject/NerdMartApplicationModule.java
@@ -2,8 +2,12 @@ package com.bignerdranch.android.initialnerdmart.inject;
 
 import android.content.Context;
 
+import com.bignerdranch.android.initialnerdmart.model.DataStore;
+import com.bignerdranch.android.initialnerdmart.model.service.NerdMartServiceManager;
 import com.bignerdranch.android.nerdmartservice.service.NerdMartService;
 import com.bignerdranch.android.nerdmartservice.service.NerdMartServiceInterface;
+
+import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -17,7 +21,20 @@ public class NerdMartApplicationModule {
     }
 
     @Provides
+    @Singleton
+    DataStore providesDataStore() {
+        return new DataStore();
+    }
+
+    @Provides
     NerdMartServiceInterface providesNerdMartServiceInterface() {
         return new NerdMartService();
+    }
+
+    @Provides
+    @Singleton
+    NerdMartServiceManager providesNerdMartServiceManager(
+            NerdMartServiceInterface serviceInterface, DataStore dataStore) {
+        return new NerdMartServiceManager(serviceInterface, dataStore);
     }
 }
